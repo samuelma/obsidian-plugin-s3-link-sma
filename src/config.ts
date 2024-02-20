b524ee2ce87f21594677daa663647371a263f2e6
samuelma <sma.samuel@gmail.com>
2024-02-20 16:05:26 +0800

    macos path use forward slash

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ export default abstract class Config {
     static readonly CACHE_FOLDER = "s3_cache";
     static readonly S3_LINK_PREFIX = "s3";
     static readonly S3_LINK_SPLITTER = ":";
-    static readonly AWS_CREDENTIALS_FILE_PATH = ".aws\\credentials";
+    static readonly AWS_CREDENTIALS_FILE_PATH = ".aws//credentials";
     static readonly AWS_PROFILE_NAME_NONE = "None";
     static readonly S3_LINK_EXPIRATION_TIME_SECONDS = 60 * 60; // 1 hour
     static readonly S3_SIGNED_LINK_PREFIX = "s3-sign";
